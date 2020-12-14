43a1002eae27c882c27717685cece49f4ef24192
Matthew RIley <matthewriley@Matthews-MacBook-Pro.local>
2020-12-14 13:32:14 -0600

    Completed Exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// 6a
+import { useState } from 'react'
+// 2a
+import MCUShows from './mcu-shows/MCUShows'
+
+// 5a
+const releaseDates = {
+  wandaVision: 'Jan 2021',
+  falconWinter: 'Mar 2021',
+  loki: 'May 2021',
+  hawkeye: 'Fall of  2021'
+ };
+
+// 6b
+ const avengers = [
+  'Scarlet Witch',
+  'Vision',
+  'Falcon',
+  'Winter Soldier',
+  'Hawkeye'
+ ];
 
 function App() {
+  // 6c
+  const [index, setIndex] = useState(0);
+
+  // 6f
+  const randomIndex = () => {
+    const newIndex =  Math.floor(Math.random() * 5);
+    setIndex(newIndex);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* 2b */}
+      <h1>FINAL REACT EXERCISE</h1>
+      {/* 2c & 5b */}
+      <MCUShows dates={releaseDates} randomIndex={randomIndex} />
+      {/* 6d */}
+      <h1>{avengers[index]}</h1>
     </div>
   );
 }
